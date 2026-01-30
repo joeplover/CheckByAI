0b2fc230210099433b8e6309008f884f03b14660
joeplover <1665637752@qq.com>
2026-01-30 13:14:42 +0800

    mysql热点redis缓存

--- Check_Ai/src/main/java/com/checkai/controller/MysqlMonitorController.java
+++ Check_Ai/src/main/java/com/checkai/controller/MysqlMonitorController.java
@@ -1,6 +1,6 @@
 package com.checkai.controller;
 
-import com.checkai.util.MysqlMonitorService;
+import com.checkai.service.MysqlMonitorService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
